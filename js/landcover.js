a62324e078f9b75e0b73c9eb587dbfceb7d61a8f
Franz Schug <sainz.fds@posteo.de>
2020-04-27 16:47:12 +0200

    fix

--- js/landcover.js
+++ js/landcover.js
@@ -36,17 +36,17 @@ function initMap() {
   position: 'topleft'
   }).addTo(map);
 
-  tilesNew = L.tileLayer('data/tv1o/{z}/{x}/{y}.png', {tms:true});
+  //tilesNew = L.tileLayer('www2data/tv1o/{z}/{x}/{y}.png', {tms:true});
   //tilesOld = L.tileLayer('data/tv1o/{z}/{x}/{y}.png', {tms:true});
-  //slovakia = L.tileLayer('data/slovakia_noBlanks/{z}/{x}/{-y}.png');
+  slovakia = L.tileLayer('https://www2.hu-berlin.de/geovis/eolab/landcover/data/slovakia_noBlanks/{z}/{x}/{-y}.png');
   //slovakia2 = L.tileLayer('data/slovakia/{z}/{x}/{y}.png', {tms:true});
  
   //map.addLayer(tilesOld)
-  map.addLayer(tilesNew)
-  //map.addLayer(slovakia)
+  //map.addLayer(tilesNew)
+  map.addLayer(slovakia)
   //map.addLayer(slovakia2)
-  tilesNew.bringToFront();
-  //slovakia.bringToFront();
+  //tilesNew.bringToFront();
+  slovakia.bringToFront();
   
   //ar = new L.GeoJSON.AJAX("data/area.geojson", {onEachFeature: onEachCountry});
   //ar.addTo(map);
